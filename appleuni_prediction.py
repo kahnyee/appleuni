12ed13adc6feed3592cbe4c86161caaa184b32d5
kahnyee <kahnyee.22@ichat.sp.edu.sg>
2024-07-11 06:46:33 +0800

    Sequential

--- appleuni_prediction.py
+++ appleuni_prediction.py
@@ -2,18 +2,53 @@ import numpy as np
 import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+import cv2
 
-
-def import_and_predict(image_data, model):
+def get_img_array(img):
     size = (75, 75)
-    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    image = ImageOps.fit(img, size, Image.LANCZOS)
     image = image.convert('RGB')
     image = np.asarray(image)
     image = (image.astype(np.float32) / 255.0)
-    img_reshape = image[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
-    return prediction
+    img_array = np.expand_dims(image, axis=0)
+    return img_array
 
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    heatmap = heatmap.numpy()
+    return heatmap
+
+def import_and_predict(image_data, model):
+    img_array = get_img_array(image_data)
+    prediction = model.predict(img_array)
+    heatmap = make_gradcam_heatmap(img_array, model, 'conv2d_4')  # Use the name of your last convolutional layer
+    return prediction, heatmap
+
+def save_and_display_gradcam(img, heatmap, cam_path="cam.jpg", alpha=0.4):
+    img = ImageOps.fit(img, (75, 75), Image.LANCZOS)
+    img = img.convert('RGB')
+    img = np.array(img)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = cv2.addWeighted(heatmap, alpha, img, 1 - alpha, 0)
+    cv2.imwrite(cam_path, superimposed_img)
+    return cam_path
 
 # Loading a trained model
 model_path = "C:/Users/kahny/ML Model/97bestmodel.h5"
@@ -30,7 +65,7 @@ if file is None:
 else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
+    prediction, heatmap = import_and_predict(image, model)
 
     label_map = {0: "It is an apple!", 1: "It is a uni sushi!", 2: "It is unknown!"}
     predicted_label = np.argmax(prediction)
@@ -38,3 +73,7 @@ else:
 
     st.text("Probability (0: Apple, 1: Uni, 2: Unknown)")
     st.write(prediction)
+
+    # Display heatmap
+    cam_path = save_and_display_gradcam(image, heatmap)
+    st.image(cam_path, caption='Grad-CAM Heatmap', use_column_width=True)
--- cnn.py
+++ cnn.py
@@ -1,8 +1,8 @@
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input, concatenate, GlobalAveragePooling2D, Layer, Activation
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Input, concatenate
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback, LearningRateScheduler
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, Callback
 from tensorflow.keras import regularizers
 import matplotlib.pyplot as plt
 import os
@@ -16,16 +16,6 @@ batch_size = 15
 initial_learning_rate = 1.5e-4
 l2_regularize = 1.5e-5
 l1_regularize = 1e-6
-
-con_layer = 4
-con_filters = "(64,128,256,512)"
-
-dense_layer = 3
-drop_out_rate = "(0.1,0.1,0.1)"
-dense_filters = "(64,32,16)"
-
-regularizer = 4
-data_aug = True
 
 # Directories
 root = 'C:/Users/kahny/ML Model'
@@ -66,98 +56,49 @@ def image_gen_w_aug(train_parent_directory, validate_parent_directory, test_pare
 
     return train_generator, val_generator, test_generator
 
-def inception_module(x, filters):
-    f1, f2, f3, f4 = filters
-    
-    conv1 = Conv2D(f1, (1, 1), padding='same', activation='relu')(x)
-    
-    conv3 = Conv2D(f2[0], (1, 1), padding='same', activation='relu')(x)
-    conv3 = Conv2D(f2[1], (3, 3), padding='same', activation='relu')(conv3)
-    
-    conv5 = Conv2D(f3[0], (1, 1), padding='same', activation='relu')(x)
-    conv5 = Conv2D(f3[1], (5, 5), padding='same', activation='relu')(conv5)
-    
-    pool = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(x)
-    pool = Conv2D(f4, (1, 1), padding='same', activation='relu')(pool)
-    
-    out = concatenate([conv1, conv3, conv5, pool], axis=-1)
-    return out
+train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, val_dir, test_dir)
 
-def residual_block(x, filters, kernel_size=3):
-    shortcut = Conv2D(filters, (1, 1), padding='same')(x)  # Adjust the number of filters in the shortcut
-    x = Conv2D(filters, kernel_size, padding='same', activation='relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(filters, kernel_size, padding='same', activation=None)(x)
-    x = BatchNormalization()(x)
-    x = tf.keras.layers.add([shortcut, x])
-    x = Activation('relu')(x)
-    return x
+def build_sequential_inception(input_shape):
+    model = Sequential()
+    
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D((2, 2)))
 
-class AttentionModule(Layer):
-    def __init__(self, **kwargs):
-        super(AttentionModule, self).__init__(**kwargs)
-        self.dense1 = Dense(units=None, activation='relu')  # units will be set dynamically
-        self.dense2 = Dense(units=None, activation='sigmoid')  # units will be set dynamically
+    # First Inception-like block
+    model.add(Conv2D(32, (1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(48, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
 
-    def build(self, input_shape):
-        self.dense1.units = input_shape[-1] // 8
-        self.dense2.units = input_shape[-1]
-        super(AttentionModule, self).build(input_shape)
+    # Second Inception-like block
+    model.add(Conv2D(64, (1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(96, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
 
-    def call(self, x):
-        avg_pool = tf.reduce_mean(x, axis=[1, 2], keepdims=True)
-        max_pool = tf.reduce_max(x, axis=[1, 2], keepdims=True)
-        concat = concatenate([avg_pool, max_pool], axis=-1)
-        dense1_output = self.dense1(concat)
-        dense2_output = self.dense2(dense1_output)
-        return x * dense2_output
+    # Third Inception-like block
+    model.add(Conv2D(128, (1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(192, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
 
-def mixup_data(x, y, alpha=0.2):
-    if alpha > 0:
-        lam = np.random.beta(alpha, alpha)
-    else:
-        lam = 1
-    batch_size = x.shape[0]
-    index = np.random.permutation(batch_size)
-    mixed_x = lam * x + (1 - lam) * x[index, :]
-    mixed_y = lam * y + (1 - lam) * y[index, :]
-    return mixed_x, mixed_y
+    # Fourth Inception-like block
+    model.add(Conv2D(128, (1, 1), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(192, (5, 5), padding='same', activation='relu'))
+    model.add(MaxPooling2D((2, 2)))
 
-def cosine_annealing(epoch):
-    return initial_learning_rate * (1 + np.cos(np.pi * epoch / epochs)) / 2
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=l1_regularize, l2=l2_regularize)))
+    model.add(Dropout(0.1))
+    model.add(Dense(128, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=l1_regularize, l2=l2_regularize)))
+    model.add(Dropout(0.1))
+    model.add(Dense(3, activation='softmax'))
 
-def build_model(input_shape):
-    input_layer = Input(shape=input_shape)
-    
-    x = Conv2D(32, (3, 3), padding='same', activation='relu')(input_layer)
-    x = BatchNormalization()(x)
-    x = MaxPooling2D((2, 2))(x)
-    
-    x = inception_module(x, [32, (48, 64), (8, 16), 16])
-    x = MaxPooling2D((2, 2))(x)
-    x = residual_block(x, 64)
-    
-    x = inception_module(x, [64, (96, 128), (16, 32), 32])
-    x = MaxPooling2D((2, 2))(x)
-    x = AttentionModule()(x)
-    
-    x = inception_module(x, [128, (128, 192), (32, 96), 64])
-    x = MaxPooling2D((2, 2))(x)
-    x = residual_block(x, 128)
-    
-    x = inception_module(x, [128, (128, 192), (32, 96), 64])
-    x = MaxPooling2D((2, 2))(x)
-    
-    x = Flatten()(x)
-    x = Dense(256, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=l1_regularize, l2=l2_regularize))(x)
-    x = Dropout(0.3)(x)
-    x = Dense(128, activation='relu', kernel_regularizer=regularizers.l1_l2(l1=l1_regularize, l2=l2_regularize))(x)
-    x = Dropout(0.2)(x)
-    
-    output_layer = Dense(3, activation='softmax')(x)
-    
-    model = Model(input_layer, output_layer)
     return model
+
+input_shape = (75, 75, 3)
+model = build_sequential_inception(input_shape)
 
 class CustomStopper(Callback):
     def on_epoch_end(self, epoch, logs=None):
@@ -165,20 +106,49 @@ class CustomStopper(Callback):
         val_acc = logs.get('val_accuracy')
         if train_acc is not None and val_acc is not None:
             if train_acc >= 0.97 and val_acc >= 0.97:
-                print(f"\nStopping training as both train and val accuracy have reached 97%.")
                 self.model.stop_training = True
+                
+def plot_misclassified_images(generator, true_labels, predicted_classes, class_labels, img_dir, title):
+    # Find the indices of misclassified images
+    misclassified_indices = np.where(predicted_classes != true_labels)[0]
+    
+    # Number of misclassified images
+    num_misclassified = len(misclassified_indices)
+    print(f"Number of misclassified images in {title}: {num_misclassified}")
+    
+    if num_misclassified > 0:
+        max_images_per_plot = 9
+        num_plots = int(np.ceil(num_misclassified / max_images_per_plot))
+        
+        for plot_num in range(num_plots):
+            start_idx = plot_num * max_images_per_plot
+            end_idx = start_idx + max_images_per_plot
+            plot_indices = misclassified_indices[start_idx:end_idx]
+            
+            num_images_in_plot = len(plot_indices)
+            rows = int(np.ceil(num_images_in_plot / 3))
+            cols = 3  # Fixed number of columns
+            
+            plt.figure(figsize=(15, 15))
+            for i, idx in enumerate(plot_indices):
+                plt.subplot(rows, cols, i + 1)
+                img_path = os.path.join(img_dir, generator.filenames[idx])
+                img = plt.imread(img_path)
+                plt.imshow(img)
+                plt.title(f"True: {class_labels[true_labels[idx]]}\nPred: {class_labels[predicted_classes[idx]]}")
+                plt.axis('off')
+            plt.suptitle(f"{title} - Plot {plot_num + 1}")
+            plt.tight_layout()
+            plt.show()
+    else:
+        print(f"No misclassified images to display in {title}.")
 
-train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, val_dir, test_dir)
-
-input_shape = (75, 75, 3)
-model = build_model(input_shape)
 model.compile(optimizer=Adam(learning_rate=initial_learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Callbacks
 early_stop = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5)
 custom_stopper = CustomStopper()
-lr_scheduler = LearningRateScheduler(cosine_annealing)
 
 # Function to convert DirectoryIterator to tf.data.Dataset
 def generator_to_dataset(generator):
@@ -195,15 +165,19 @@ def generator_to_dataset(generator):
 train_dataset = generator_to_dataset(train_generator).repeat()
 validation_dataset = generator_to_dataset(validation_generator).repeat()
 
+# Print model summary
+model.summary()
+
 # Training the model
-history = model.fit(
+with tf.device("/GPU:0"):
+    history = model.fit(
       train_dataset,
       steps_per_epoch=len(train_generator),
       epochs=epochs,
       validation_data=validation_dataset,
       validation_steps=len(validation_generator),
-      callbacks=[reduce_lr, custom_stopper, lr_scheduler]
-)
+      callbacks=[reduce_lr, custom_stopper]
+    )
 
 # Evaluate the model on test data
 test_loss, test_acc = model.evaluate(test_generator, verbose=1)
@@ -249,3 +223,16 @@ plt.tight_layout()
 plt.show()
 
 model.save('my_model.h5')
+
+# Plot misclassified images for training, validation, and test sets
+def evaluate_and_plot_misclassified(generator, img_dir, title):
+    generator.reset()
+    true_labels = generator.classes
+    class_labels = list(generator.class_indices.keys())
+    predictions = model.predict(generator, steps=len(generator), verbose=1)
+    predicted_classes = np.argmax(predictions, axis=1)
+    plot_misclassified_images(generator, true_labels, predicted_classes, class_labels, img_dir, title)
+
+evaluate_and_plot_misclassified(train_generator, train_dir, "Training Set")
+evaluate_and_plot_misclassified(validation_generator, val_dir, "Validation Set")
+evaluate_and_plot_misclassified(test_generator, test_dir, "Test Set")
